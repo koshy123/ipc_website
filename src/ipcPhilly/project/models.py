f4adeb78dcba92f12799ff2ce548399e406e1e2e
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-01-23 10:25:07 -0500

    add changes

---
+++ src/ipcPhilly/project/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Project(models.Model ):
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=10000)
+    contact = models.TextField()
