3c4024b2418302ea1c915c8bff334ef83b0a3346
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-01-22 21:59:04 -0500

    save branch

--- src/ipcPhilly/home/models.py
+++ src/ipcPhilly/home/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Home(models.Model ):
-    navBar = models.TextField()
-    title = models.TextField()
-    contact = models.TextField()
-    description = models.TextField(default='this is cool')
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    date = models.DecimalField()
+    contact = models.TextField(default='new member')
