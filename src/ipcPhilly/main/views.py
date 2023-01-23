b1720a13abcdd1eb0b0ea6a9eb07b090e98274e1
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-01-23 14:29:35 -0500

    having issues now, not able to find templates files

--- src/ipcPhilly/main/views.py
+++ src/ipcPhilly/main/views.py
@@ -2,14 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def main_view(*args, **kwargs):
-    return HttpResponse("<h1>Hi World</h1>")
-
-def contact_view(*arg, **kwargs):
+def main_view(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    # return HttpResponse("<h1>Hi World</h1>")
+    return render(request, "home.html", {})
+    
+def contact_view(request, *arg, **kwargs):
     return HttpResponse("<h1>Contacts Page</h1>")
 
-def about_view(*arg, **kwargs):
+def about_view(request, *arg, **kwargs):
     return HttpResponse("<h1>About Page</h1>")
 
-def calendar_view(*arg, **kwargs):
+def calendar_view(request, *arg, **kwargs):
     return HttpResponse("<h1>Calendar Page</h1>")
