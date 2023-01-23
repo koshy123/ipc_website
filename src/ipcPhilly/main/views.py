2ea0c04d97589bf4d1f9273329d19f6f80e7c0d2
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-01-23 13:23:56 -0500

    add info

--- src/ipcPhilly/main/views.py
+++ src/ipcPhilly/main/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def main_view(*args, **kwargs):
+    return HttpResponse("<h1>Hi World</h1>")
--- src/ipcPhilly/project/models.py
+++ src/ipcPhilly/project/models.py
@@ -4,4 +4,5 @@ class Project(models.Model ):
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(decimal_places=2, max_digits=10000)
-    contact = models.TextField()
+    contact = models.TextField(blank=False, null=False)
+    join = models.BooleanField()
